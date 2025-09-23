8dc2a667eda0b7784dee3057689766d70a9bc02d
LeGenda <151869637+LeGenda3107@users.noreply.github.com>
2025-09-23 12:54:47 +0300

    Create calculator.sh

---
+++ bash_src/calculator.sh
@@ -0,0 +1,34 @@
+# !/bin/bash
+
+clear
+# Take user Input
+cowsay -f tux "Enter first number : " | lolcat
+read a
+clear
+
+cowsay -f tux -n << "EOF" | lolcat
+Enter Choice :
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+EOF
+read ch
+
+clear
+cowsay -f tux  "Enter second number : " | lolcat
+read b
+clear
+# Switch Case to perform
+# calculator operations
+case $ch in
+  1)res=`echo $a + $b | bc`
+  ;;
+  2)res=`echo $a - $b | bc`
+  ;;
+  3)res=`echo $a \* $b | bc`
+  ;;
+  4)res=`echo "scale=2; $a / $b" | bc`
+  ;;
+esac
+cowsay -f tux "Result : $res"
